e8a0e7a7d932ae897d762922241050ef037cee78
ignacio <ignacio_lencina@estudante.sesisenai.org.br>
2026-04-10 12:14:22 -0300

    add - commit final

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ e.preventDefault();
 
 let nome = document.getElementById("nome").value;
 let idade = document.getElementById("idade").value;//idade
+let temp = document.getElementById("temp").value;//tempo sozinho
 let telefone = document.getElementById("telefone").value;//telefone
 let cpf = document.getElementById("cpf").value;//cpf
 let motivo = document.getElementById("motivo").value;//motivo
@@ -15,13 +16,15 @@ let apt = document.querySelector('input[name="apt"]:checked');//ja teve
 
 
 if(idade <18 ) return alert("idade ruim")
+    if(temp <8 ) return alert("tempo ruim")
 
 if(telefone.length < 8) return alert("telefone Inválido");
 if(cpf.length == 0) return alert("cpf Inválido");
 if(motivo.length < 10) return alert("motivo Inválido");
 if(jateve.value == "naoteve") return alert("tem q ter tido");
 if(apt.value == "aptnao") return alert("tem q te");
-''
+
+
 if(nome.length < 3) return alert("Nome Inválido");
 document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "nome" + nome;
 
